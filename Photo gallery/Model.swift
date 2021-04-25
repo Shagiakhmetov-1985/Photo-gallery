13f9603ca7487979ba89e8a37d3007aee26c3f44
Marat <Shagiakhmetov2499@gmail.com>
2021-04-25 16:58:03 +0300

    setup elements

---
+++ Photo gallery/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  Photo gallery
+//
+//  Created by Marat Shagiakhmetov on 25.04.2021.
+//
+
+struct Cars {
+    
+    let name = [
+        "Audi A3", "Audi A4", "Audi A6", "Audi Q3", "Audi Q5", "Audi Q7"
+    ]
+    
+    let image = [
+    "AudiA3", "AudiA3(2)", "AudiA4", "AudiA4(2)", "AudiA6", "AudiA6(2)",
+        "AudiQ3", "AudiQ3(2)", "AudiQ5", "AudiQ5(2)", "AudiQ7", "AudiQ7(2)"
+    ]
+    
+}
